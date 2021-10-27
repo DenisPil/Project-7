8de819236f255057d3ee5dac02a3651e6034d5c7
DenisPil <denis.pilongery@gmail.com>
2021-10-27 11:41:55 +0200

    ajout des prints

--- optimized.py
+++ optimized.py
@@ -2,34 +2,18 @@ import time
 import csv
 
 
-"""
-Calcul le temps d'execution :
-start_time = représente le début du programme
-"""
 start_time = time.time()
-
-"""
-Création des listes:
-name = le nom des actions
-action_value = la valeur des actions
-benefit = le bénéfice attendu par action 
-"""
 list_actions = list()
 name = list()
 actions = list()  #    wts
 benefit = list() #vals
 
-f= open ("actions_premiere_partie.csv")
+f = open ("actions_premiere_partie.csv")
 myReader = csv.reader(f)
 for row in myReader:
     list_actions.append(row)
 del list_actions[0]
 
-
-"""
-Boucle qui va extraire les informations de la liste de tuple  "actions"
-et ajouter les données aux listes
-"""
 for i in range(len(list_actions)):
     n, v, p = list_actions[i]
     value = int(v) * 100
@@ -38,65 +22,46 @@ for i in range(len(list_actions)):
     name.append(n)
     actions.append(value)
 
-
-"""
-Création des variables :
-capacity = le maximum pour acheter des actions
-max_value = représente les colonnes du tableau
-num_of_actions = le nombre d'actions
-"""
 cap = 50000
 capacity = cap + 1
 num_of_actions = len(actions)
 
+table = [[0 for i in range(capacity)] for elem in range(num_of_actions)]
+index = [["" for i in range(capacity)] for elem in range(num_of_actions)]
+for row in range(len(actions)):
+    for column in range(capacity):
 
-"""
-Création du tableau:
-table = une liste qui contient autant de listes qu'il y a d'actions et ces listes contiennent autant
-d'éléments que la capacité maximum.
-Le tableau :  "lignes * nb-actions " et "colonnes * capacité"
-"""
-tableau = [[0 for i in range(capacity)] for elem in range(num_of_actions)]
-nomtab = [["" for i in range(capacity)] for elem in range(num_of_actions)]
+        if actions[row] > column:
+            table[row][column] = table[row - 1][column]
+            index[row][column] = index[row - 1][column]
 
-"""
-La boucle est créée à partir de la liste "action_value" et va itérer sur chaque lignes
-"""
-for ligne in range(len(actions)):
-
-    """La boucle est créée à partir de la capacité maximale et va itérer sur chaque colonnes"""
-    for colonne in range(capacity):
-
-        """
-        Si la valeur de l'action est plus élevée que la capacité de la colonne, 
-        On récupère la valeur de la cellule précédente (de la colonne)
-        """
-        if actions[ligne] > colonne:
-            tableau[ligne][colonne] = tableau[ligne - 1][colonne]
-            nomtab[ligne][colonne] = nomtab[ligne - 1][colonne]
             continue
+        prior_value = table[row - 1][column]
+        new_option_best = benefit[row] + table[row - 1][column - actions[row]]
 
-        """
-        Si la valeur de l'action est < à la capacité.
-        prior_value = l'élément précédent de la colonne
-        new_option_best = la valeur de l'action en cours de traitement + la valeur............. 
-        """
-        prior_value = tableau[ligne - 1][colonne]
-        new_option_best = benefit[ligne] + tableau[ligne - 1][colonne - actions[ligne]]
-
-        prior_name = nomtab[ligne - 1][colonne]
-        best = ', ' + nomtab[ligne - 1][colonne - actions[ligne]]
-        new_name_best = name[ligne] + best
+        prior_index = index[row - 1][column]
+        best_index = index[row - 1][column - actions[row]]
+        new_index_best = str(row) + " " + str(best_index)
 
         if prior_value < new_option_best :
-            tableau[ligne][colonne] = new_option_best
-            nomtab[ligne][colonne] = new_name_best
+            table[row][column] = new_option_best
+            index[row][column] = new_index_best
         else:
-            tableau[ligne][colonne] = prior_value
-            nomtab[ligne][colonne] = prior_name
+            table[row][column] = prior_value
+            index[row][column] = prior_index
 
-result = max([x for y in tableau for x in y])
-result_name = len(nomtab) - 1
-print(nomtab[result_name][-1])
-print(result / 100)
+result = max([x for y in table for x in y])
+len_index = len(index) - 1
+rez = index[len_index][-1].split()
+
+result_name = list()
+result_price = list()
+for i in rez:
+    elem = int(i)
+    result_name.append(name[elem])
+    result_price.append(actions[elem])
+
+print("Pour un total de :", sum(result_price) / 100, "€")
+print("Les actions les plus rentables sont :", ', '.join(result_name))
+print("Pour un bénéfice de :", result / 100, "€" )
 print("--- %s seconds ---" % (time.time() - start_time))
